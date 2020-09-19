1c1d6f63ff034e6bf33a32c87722a66841c02559
1234831 <71382664+1234831@users.noreply.github.com>
2020-09-19 04:17:39 -1100

    complete project 1 with html css and js
    completed whole project 1

--- p01/script.js
+++ p01/script.js
@@ -20,9 +20,14 @@ function showSuccess(input) {
 }
 //function to check is email is valid or not
 
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, `Please provide a valid email`)
+    }
+
 }
 
 //function to check if require fields have data
@@ -36,12 +41,34 @@ function checkRequired(inputArray) {
     })
 
 }
+
+
+
+// function to check length of the fields
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldId(input)} needs to be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldId(input)} needs to be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+
+    }
+}
+
+// if password and confirm password match
+
+function checkpassword(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, "please enter a correct password")
+    }
+}
+
+
 //function to get the field of the id in proper case
 function getFieldId(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
-
-
 
 
 //This is an event listener form on submit
@@ -49,5 +76,9 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 10);
+    checkLength(password, 10, 30);
+    checkEmail(email);
+    checkpassword(password, password2);
 
 })
