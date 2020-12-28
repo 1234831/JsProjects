133e6fa0d8ecae9ccd3f8501766197e859a7c82e
shams khan <shamasali305@gamil.com>
2020-12-28 18:49:39 +0500

    complete JS for project 20
    javascript complete for final project
    and some change in css

--- p20/script.js
+++ p20/script.js
@@ -1,3 +1,65 @@
-662 b4c6f5d72d5fef3feeee0e604cc66
+//weather api //
 
-api.openweathermap.org / data / 2.5 / weather ? q = { city name } & appid = { API key }
+const weatherApi = {
+    key: "662b4c6f5d72d5fef3feeee0e604cc66",
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather"
+}
+
+const searchInputBox = document.getElementById('inputBox');
+
+
+//Eventlistner function on keypress
+
+
+searchInputBox.addEventListener('keypress', (event) => {
+    if (event.keyCode === 13) {
+        console.log(searchInputBox.value);
+        getWeatherReport(searchInputBox.value);
+        document.querySelector('.weather-body').style.display = 'block';
+    }
+});
+
+//Get weather report 
+function getWeatherReport(city) {
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+        .then(weather => {
+            return weather.json();
+        }).then(showWeatherReport);
+}
+//show Weather report 
+function showWeatherReport(weather) {
+    console.log(weather);
+
+    let city = document.getElementById('city');
+    city.innerText = `${weather.name} ${weather.sys.country}`;
+
+    let temperature = document.getElementById('temp');
+    temperature.innerHTML = `${Math.round(weather.main.temp)} &deg;C`;
+
+    let minMaxTemp = document.getElementById('min-max');
+    minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)} &deg;C (min)/ ${Math.ceil(weather.main.temp_min)}  &deg;C (max) `;
+
+
+    let weatherType = document.getElementById('weather');
+    weatherType.innerText = `${weather.weather[0].main}`;
+
+    let date = document.getElementById('date');
+    let todayDate = new Date();
+    date.innerText = dateManage(todayDate);
+}
+//Date manage
+
+function dateManage(dateArg) {
+
+    let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+
+    return `${date} ${month} (${day}), ${year}`;
+
+}
